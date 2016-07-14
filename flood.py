e99019654c7e57b15fb329f0cb144e19be4f3f95
Daniel Ruswick <dan.ruswick@tradingtechnologies.com>
2016-07-14 15:01:00 -0500

    init

---
+++ flood.py
@@ -0,0 +1,125 @@
+import os
+import subprocess
+from flask import *
+import time
+import json
+from threading import Thread
+
+
+class FlashFlooder():
+    CAPTURE_DIR = '/var/captures'
+    CAPTURE_FILE = '/var/captures/{0}'
+    EXTRACTED_DIR = './extracted_erfs'
+    EXTRACTED_FILE = './extracted_erfs/data_{0}.erf'
+    decompress_proc = None
+    flood_proc = None
+
+
+    def flood(self, lzo_data=None, duration=None, speed=0):
+        # to prevent this script from using too much storage, we automatically clean out the uncompressed files once they take up 20 gb
+        if self.getStorageUsage() > 20000000000:
+            print('Cleaning out excess stored data')
+            subprocess.call('sudo rm -r {0}'.format(self.EXTRACTED_DIR), shell=True)
+            subprocess.call('sudo mkdir {0}'.format(self.EXTRACTED_DIR), shell=True)
+
+        # if no decompressed file is specified, we take the compressed file and decompress it
+        lzo_data = self.CAPTURE_FILE.format(self.getIndex()[0] if not lzo_data else lzo_data)
+        timestamp = int(time.time())
+        decompressed_data = self.EXTRACTED_FILE.format(str(timestamp))
+        decompression_opts = ' '.join(['sudo', '../run', 'lzop', '-d', '-o', decompressed_data, '-f', lzo_data])
+
+        self.decompress_proc = subprocess.Popen(decompression_opts, shell=True)
+        self.decompress_proc.wait()
+        self.decompress_proc = None
+
+        flood_opts = ['sudo', '../run', 'dagflood', '-f', decompressed_data, '-d', 'dag0']
+        if duration:
+            flood_opts.extend(['-t', str(duration)])
+        # 0 will replay live, a positive number will play at that number of mbit, and a negative number or null will replay at line rate
+        if speed == 0:
+            flood_opts.append('-R')
+        elif speed > 0:
+            flood_opts.extend(['-w', str(speed) + 'm'])
+        flood_opts.append('-v')
+        self.flood_proc = subprocess.Popen(' '.join(flood_opts), shell=True)
+        self.flood_proc.wait()
+        self.flood_proc = None
+
+    def isRunning(self):
+        return (self.flood_proc or self.decompress_proc)
+
+    def term(self):
+        if self.decompress_proc:
+            # this throws a system permissions error if I try to use proc.terminate(), so we have to force it
+            os.system("sudo kill {0}".format(str(self.decompress_proc.pid)))
+            self.decompress_proc = None
+        time.sleep(0.1)
+        if self.flood_proc:
+            os.system("sudo kill {0}".format(str(self.flood_proc.pid)))
+            self.flood_proc = None
+
+    @staticmethod
+    def getStorageUsage():
+        folder_size = 0
+        for (path, dirs, files) in os.walk(FlashFlooder.EXTRACTED_DIR):
+            for file in files:
+                filename = os.path.join(path, file)
+                folder_size += os.path.getsize(filename)
+        return folder_size
+
+    @staticmethod
+    def getIndex():
+        return os.listdir(FlashFlooder.CAPTURE_DIR)
+
+def process_request_thread(flooder, l, d, s):
+    flooder.flood(l, d, s)
+
+
+app = Flask(__name__)
+
+global f
+f = FlashFlooder()
+
+
+@app.route('/', methods=['GET'])
+def root():
+    return render_template('index.html')
+
+
+@app.route('/flood', methods=['POST'])
+def flood():
+    data = request.form
+    print('Processing request with parameters {0}'.format(data))
+    file = data['file'] if 'file' in data else None
+    duration = data['duration'] if 'duration' in data else 30
+    speed = data['speed'] if 'speed' in data else 100
+    t = Thread(target=process_request_thread, args=(f, file, duration, speed))
+    t.start()
+    return json.dumps({'status': 'initialized', 'message': 'the system is initialized and will begin flooding momentarily'})
+
+
+@app.route('/kill', methods=['POST'])
+def kill():
+    if f.isRunning():
+        f.term()
+        return json.dumps({'status': 'killed', 'message': 'the system process has been killed'})
+    else:
+        return json.dumps({'status': 'inactive', 'message': 'the system is currently not active'})
+
+
+@app.route('/status', methods=['POST'])
+def status():
+    if f.decompress_proc:
+        return json.dumps({'status': 'decompressing', 'message': 'the system is currently decompressing the data'})
+    elif f.flood_proc:
+        return json.dumps({'status': 'flooding', 'message': 'the system is currently flooding the data'})
+    else:
+        return json.dumps({'status': 'inactive', 'message': 'the system is currently not active'})
+
+@app.route('/file_index', methods=['POST'])
+def file_index():
+        return json.dumps({'files': FlashFlooder.getIndex()})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ static/script.js
@@ -0,0 +1,216 @@
+        var IntervalTimer = null;
+        function textToSeconds(exp){
+                hours = 0
+                minutes = 0
+                seconds = 0
+
+                if(h = /(\d{1,2})\W?(hour(s)?|h)(\b)/g.exec(exp)){
+                    hours += parseInt(h[1]);
+                }
+                if(h = /(\d{1,3})\W?(minute(s)?|m|min)(\b)/g.exec(exp)){
+                     minutes += parseInt(h[1]);
+                }
+                if(h = /(\d{1,3})\W?(second(s)?|s|sec)(\b)/g.exec(exp)){
+                     seconds += parseInt(h[1]);
+                }
+                if(h = /([a-z]+)\W?(hour(s)?|h)(\b)/g.exec(exp)){
+                    hours += wordToNum(h[1]);
+                }
+                if(h = /([a-z]+)\W?(minute(s)?|m|min)(\b)/g.exec(exp)){
+                     minutes += wordToNum(h[1]);
+                }
+                if(h = /([a-z]+)\W?(second(s)?|s|sec)(\b)/g.exec(exp)){
+                     seconds += wordToNum(h[1]);
+                }
+                if(h=/(\d{1,2}):(\d{2})(:(\d{2}))?/g.exec(exp)){
+                    hours += parseInt(h[1]);
+                    minutes += parseInt(h[2]);
+                    if(h.length == 5){
+                        seconds += parseInt(h[4])
+                    }
+
+                }
+                return seconds + 60*minutes + 3600*hours;
+            }
+            function secondsToText(secs){
+                h = Math.floor(secs / 3600);
+                m = secs % 3600;
+                m = Math.floor(m / 60);
+                s = secs % 60;
+                return h + ':' + pad(m) + ':' + pad(s);
+            }
+            function pad(num) {
+                return num < 10 ? '0' + num : num;
+            }
+            function wordToNum(word){
+                var n = {
+                    'one' : 1,
+                    'two': 2,
+                    'three': 3,
+                    'four' : 4,
+                    'five': 5,
+                    'six' : 6,
+                    'seven' : 7,
+                    'eight' : 8,
+                    'nine' : 9
+                }
+                if(word in n){
+                    return n[word];
+                }
+                else{
+                    return 0;
+                }
+            }
+        function updateView(status){
+
+                var b = $('#flood-button');
+                var fs = $('#flood-status');
+                var fst =  $('#flood-status-text');
+                if(status == 'flooding'){
+                    $('#flood-speed-select').slider('disable');
+                    $('#flood-speed-custom').slider('disable');
+                    $("#capture-select").prop('disabled', true);
+                    $("#flood-duration").prop('disabled', true);
+
+                    b.removeClass().addClass('btn btn-danger');
+                    b.html('Kill Flood');
+                    fs.removeClass().addClass('active');
+                    fst.html('FlashFlood is active');
+                    if(!IntervalTimer){
+                        IntervalTimer = setInterval(function(e){
+                            $.post('/status').done(function(e){
+                                updateView(JSON.parse(e)['status'])
+                            });
+                        }, 3000);
+                    }
+                }
+                if(status == "initialized"){
+                    $('#flood-speed-select').slider('disable');
+                    $('#flood-speed-custom').slider('disable');
+                    $("#capture-select").prop('disabled', true);
+                    $("#flood-duration").prop('disabled', true);
+
+                    b.removeClass().addClass('btn btn-danger');
+                    b.html('Kill Flood');
+                    fs.removeClass().addClass('initialized');
+                    fst.html('FlashFlood is initialized and will start soon');
+                    if(!IntervalTimer){
+                        IntervalTimer = setInterval(function(e){
+                            $.post('/status').done(function(e){
+                                updateView(JSON.parse(e)['status'])
+                            });
+                        }, 3000);
+                    }
+                }
+                if(status == "decompressing"){
+                    $('#flood-speed-select').slider('disable');
+                    $('#flood-speed-custom').slider('disable');
+                    $("#capture-select").prop('disabled', true);
+                    $("#flood-duration").prop('disabled', true);
+
+                    b.removeClass().addClass('btn btn-danger');
+                    b.html('Kill Flood');
+                    fs.removeClass().addClass('decompresing');
+                    fst.html('FlashFlood is decompressing the data');
+                    if(!IntervalTimer){
+                        IntervalTimer = setInterval(function(e){
+                            $.post('/status').done(function(e){
+                                updateView(JSON.parse(e)['status'])
+                            });
+                        }, 3000);
+                    }
+                }
+                if(status == "killed"){
+                    $('#flood-speed-select').slider('disable');
+                    $('#flood-speed-custom').slider('disable');
+                    $("#capture-select").prop('disabled', true);
+                    $("#flood-duration").prop('disabled', true);
+
+                    b.html('Flood Killed');
+                    fs.removeClass().addClass('killed');
+                    fst.html('FlashFlood has been killed');
+                    clearInterval(IntervalTimer);
+                    IntervalTimer = null;
+                    setTimeout(function(e){
+                        $('#flood-speed-select').slider('enable');
+                        $('#flood-speed-custom').slider('enable');
+                        $("#capture-select").prop('disabled', false);
+                        $("#flood-duration").prop('disabled', false);
+
+                        b.removeClass().addClass('btn btn-primary');
+                        b.html('Begin Flood');
+                        fs.addClass('hidden');
+                        fst.html('FlashFlood not yet started');
+                    }, 1000);
+                }
+                if(status == "inactive"){
+                        $('#flood-speed-select').slider('enable');
+                        $('#flood-speed-custom').slider('enable');
+                        $("#capture-select").prop('disabled', false);
+                        $("#flood-duration").prop('disabled', false);
+
+                        b.html('Begin Flood');
+                        b.removeClass().addClass('btn btn-primary');
+                        fs.addClass('hidden');
+                        fst.html('FlashFlood has not yet started');
+                        clearInterval(IntervalTimer);
+                        IntervalTimer = null;
+                    }
+            }
+        $(function(){
+            $('#flood-speed-select').slider({
+            ticks: [-1,0,1],
+            ticks_labels: ['Line Rate', 'Live Replay', 'Custom Rate'],
+                tooltip: 'hide'
+            });
+            $('#flood-speed-custom').slider({
+                min: 10,
+                max: 5000,
+                scale: 'logarithmic',
+                step: 10,
+                formatter: function(value) {return value >= 1000 ? value/1000 + 'gbit' : value + 'mbit';},
+            });
+            $('#flood-speed-select').val() == '1' ? $('#flood-speed-custom').slider('enable') : $('#flood-speed-custom').slider('disable');
+            $('#flood-speed-select').on('change', function(e){e.target.value == '1' ? $('#flood-speed-custom').slider('enable') : $('#flood-speed-custom').slider('disable');})
+            $.post('/file_index').done(function(data){
+                var options = $("#capture-select");
+                $.each(JSON.parse(data)['files'], function(e){
+                    options.append($("<option />").val(this).text(this));
+                });
+            });
+            $('#flood-duration').keyup(function(e){
+                var v = textToSeconds($(e.target).val());
+                if(v > 0){
+                    $('#formatted-time').html(secondsToText(v));
+                    $('#formatted-time').data('seconds', v);
+                }
+                else if($(e.target).val() > 0){
+                    $('#formatted-time').html("...");
+                    $('#formatted-time').data('seconds', 0);
+                }
+                else{
+                    $('#formatted-time').html("");
+                    $('#formatted-time').data('seconds', 0);
+                }
+            });
+            $('#flood-button').click(function(e){
+                var fb = $(e.target);
+                if(fb.hasClass('btn btn-primary')){
+                    $.ajax('/flood', {data: {file: $("#capture-select").val(),
+                        duration: $('#formatted-time').data('seconds'),
+                        speed: ($('#flood-speed-select').val() == "1" ? $('#flood-speed-custom').val() : $('#flood-speed-select').val())}, type: 'POST'
+                    }).done(function(f){
+                        updateView(JSON.parse(f)['status']);
+                    });
+                }
+                else{
+                    $.post('/kill').done(function(f){
+                        console.log(f);
+                        updateView(JSON.parse(f)['status']);
+                    });
+                }
+            });
+             $.post('/status').done(function(e){
+                                updateView(JSON.parse(e)['status'])
+                            });
+        });
